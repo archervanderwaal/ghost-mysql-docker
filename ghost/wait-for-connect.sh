2f8e7e4e248e09fde8b6e7e9acd46511cda9f2f8
archer <archer.vanderwaal@gmail.com>
2019-03-08 19:39:43 +0800

    rebuild

--- ghost/wait-for-connect.sh
+++ ghost/wait-for-connect.sh
@@ -3,9 +3,12 @@ set -e
 host="$1"
 shift
 cmd="$@"
-until mysql -h"${MYSQL_HOST}" -p"3306" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" ; do
+echo $cmd
+echo "${MYSQL_USER} ${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
+until mysql -hdb -p"3306" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" ; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
 >&2 echo "MySQL is up - executing command"
+echo $@
 exec "$@"
