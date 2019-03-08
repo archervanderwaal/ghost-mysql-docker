09011a8f48c975e08ac391bc5de227e225f407e0
archer <archer.vanderwaal@gmail.com>
2019-03-08 22:25:38 +0800

    Modify wait-for-connect.sh

--- ghost/wait-for-connect.sh
+++ ghost/wait-for-connect.sh
@@ -4,8 +4,7 @@ host="$1"
 shift
 cmd="$@"
 echo $cmd
-echo "${MYSQL_USER} ${MYSQL_PASSWORD} ${MYSQL_DATABASE}"
-until mysql -hdb -p"3306" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -D"${MYSQL_DATABASE}" ; do
+until mysql -hdb -p"3306" -u"${database__connection__user}" -p"${database__connection__password}" -D"${database__connection__database}" ; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
